8cfc39ec0b22edb395395e493429becd1ac6f747
mrdeepu0547 <saideepak.sabbineni@syenainfosoft.com>
2025-07-01 01:47:57 +0530

    final code for counter

--- counter-app/src/app/app.module.ts
+++ counter-app/src/app/app.module.ts
@@ -4,6 +4,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CounterComponent } from './counter/counter.component';
+import { NgxSpinnerModule } from 'ngx-spinner';
 
 @NgModule({
   declarations: [
@@ -12,7 +13,8 @@ import { CounterComponent } from './counter/counter.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    NgxSpinnerModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- counter-app/src/app/counter/counter.component.ts
+++ counter-app/src/app/counter/counter.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { NgxSpinnerService } from 'ngx-spinner';
 
 @Component({
   selector: 'app-counter',
@@ -6,10 +7,51 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./counter.component.css']
 })
 export class CounterComponent implements OnInit {
+  constructor(
+    public spinner: NgxSpinnerService){
 
-  constructor() { }
-
+  }
   ngOnInit(): void {
   }
-
+  counter = 0;
+  history: string[] = [];
+  animated = false;
+  //Increses the value of counter by one unit
+  increment() {
+    this.spinner.show('spinner');
+    this.counter++;
+    this.history.unshift(`[+1] Counter is now ${this.counter}`);
+    this.animate();
+  }
+  //Decreases the value by one unit
+  decrement() {
+    this.spinner.show('spinner');
+    this.counter--;
+    this.history.unshift(`[-1] Counter is now ${this.counter}`);
+    this.animate();
+  }
+  //Resets the counter value by making the counter value to 0
+  reset() {
+    this.spinner.show('spinner');
+    this.counter = 0;
+    this.history.unshift(`[Reset] Counter is now 0`);
+    this.animate();
+  }
+  //Takes confirmation from user before clearing the history
+  clearHistory() {
+    this.spinner.show('spinner');
+    const confirmed = confirm('Are you sure you want to clear the history?');
+    if (confirmed) {
+      this.history = [];
+    }
+    this.spinner.hide('spinner');
+  }
+  //Provides absic animation to counter
+  animate() {
+    this.animated = true;
+    setTimeout(() => {
+      this.animated = false;
+      this.spinner.hide('spinner');
+    }, 300);
+  }
 }
